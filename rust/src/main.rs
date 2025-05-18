c66ede09b0b9d836d9c365a077d19377c1b9d724
justkira <shighgamerhg@gmail.com>
2025-05-18 04:37:36 +0300

    chore: Update .gitignore to exclude 'rust/target' directory and implement basic REPL and file execution functionality in main.rs. Remove obsolete incremental build files.

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, error::Error, fs, process::exit};
+
+fn run_file(path: &str) -> Result<(), Box<dyn Error>> {
+    let content = fs::read_to_string(path)?;
+
+    // run(content);
+
+    Ok(())
+}
+
+fn run_repl() -> Result<(), Box<dyn Error>> {
+    use std::io::{self, Write};
+
+    let mut buffer = String::new();
+
+    loop {
+        print!("> ");
+        io::stdout().flush()?;
+
+        if io::stdin().read_line(&mut buffer)? == 0 {
+            break;
+        }
+
+        if buffer.trim().is_empty() {
+            continue;
+        }
+
+        // run(&buffer);
+    }
+
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args().collect();
+    println!("Command Ran with these args {:?}", args.len() > 2);
+
+    match args.len() {
+        1 => run_repl()?,
+        2 => run_file(&args[1])?,
+        _ => {
+            eprintln!("Usage: jlox [script]");
+            exit(64);
+        }
+    }
+
+    Ok(())
 }
