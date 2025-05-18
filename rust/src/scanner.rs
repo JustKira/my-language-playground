fc73eea9e3b9d4776cf7c99bfd967ff4da9f162a
justkira <shighgamerhg@gmail.com>
2025-05-18 05:22:05 +0300

    feat: Enhance Scanner with error handling and token scanning functionality

--- rust/src/scanner.rs
+++ rust/src/scanner.rs
@@ -1,4 +1,7 @@
-#[derive(Debug)]
+use std::error::Error;
+use std::string::String;
+
+#[derive(Debug, Clone)]
 pub enum TokenType {
     // Single-character tokens.
     LeftParen,
@@ -56,7 +59,7 @@ impl std::fmt::Display for TokenType {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Literal {
     StringValue(String),
     IntValue(i64),
@@ -65,7 +68,7 @@ pub enum Literal {
     IdentifierValue(String),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Token {
     token_type: TokenType,
     lexeme: String,
@@ -112,7 +115,63 @@ impl Scanner {
         }
     }
 
-    pub fn scan_tokens(self: &Self) -> Vec<Token> {
+    pub fn scan_tokens(self: &mut Self) -> Result<Vec<Token>, Box<dyn Error>> {
+        while !self.is_at_end() {
+            self.start = self.current;
+            self.scan_token();
+        }
+
+        self.tokens.push(Token {
+            token_type: TokenType::Eof,
+            lexeme: "".to_string(),
+            literal: None,
+            line: self.line,
+        });
+
+        Ok(self.tokens.clone())
+    }
+
+    pub fn scan_token(self: &mut Self) -> Result<(), Box<dyn Error>> {
+        let c = self.advance();
+
+        match c {
+            Ok(c) => match c {
+                '(' => self.add_token(TokenType::LeftParen, None),
+                ')' => self.add_token(TokenType::RightParen, None),
+                '{' => self.add_token(TokenType::LeftBrace, None),
+                '}' => self.add_token(TokenType::RightBrace, None),
+                ',' => self.add_token(TokenType::Comma, None),
+                '.' => self.add_token(TokenType::Dot, None),
+                '-' => self.add_token(TokenType::Minus, None),
+                '+' => self.add_token(TokenType::Plus, None),
+                ';' => self.add_token(TokenType::Semicolon, None),
+                '*' => self.add_token(TokenType::Star, None),
+
+                _ => todo!(),
+            },
+            Err(e) => {
+                return Err(e);
+            }
+        }
+
         todo!()
+    }
+
+    pub fn advance(self: &mut Self) -> Result<char, Box<dyn Error>> {
+        let c = self
+            .source
+            .chars()
+            .nth(self.current)
+            .ok_or("Unexpected EOF while reading")?;
+
+        Ok(c)
+    }
+
+    pub fn add_token(self: &mut Self, token_type: TokenType, literal: Option<Literal>) {
+        todo!()
+    }
+
+    fn is_at_end(self: &Self) -> bool {
+        self.current >= self.source.len()
     }
 }
