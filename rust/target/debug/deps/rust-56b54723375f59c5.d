093d557733003c8e8c4a8a34c7a56a96fa712afd
justkira <shighgamerhg@gmail.com>
2025-05-18 03:51:47 +0300

    init

---
+++ rust/target/debug/deps/rust-56b54723375f59c5.d
@@ -0,0 +1,5 @@
+Z:\pixelscortex\my-language-playground\rust\target\debug\deps\librust-56b54723375f59c5.rmeta: src\main.rs
+
+Z:\pixelscortex\my-language-playground\rust\target\debug\deps\rust-56b54723375f59c5.d: src\main.rs
+
+src\main.rs:
---
+++ rust/target/debug/deps/rust-e9f5f573e2ed2609.d
@@ -0,0 +1,5 @@
+Z:\pixelscortex\my-language-playground\rust\target\debug\deps\librust-e9f5f573e2ed2609.rmeta: src\main.rs
+
+Z:\pixelscortex\my-language-playground\rust\target\debug\deps\rust-e9f5f573e2ed2609.d: src\main.rs
+
+src\main.rs:
